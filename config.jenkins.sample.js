e42264a00e320c04e88b3771f38c09a105e9df65
Julian Kleinhans <julian.kleinhans@aoe.com>
2015-12-15 21:26:44 +0100

    Update jenkins config sample

--- config.jenkins.sample.js
+++ config.jenkins.sample.js
@@ -22,48 +22,46 @@ module.exports = {
                 displayDuration_isEnabled: true
             },
 
-            //parameterizedAttributes: [
-            //    {
-            //        attributeName: 'branch',
-            //        jenkinsParameterName: 'BRANCH_TO_BUILD'
-            //    }
-            //]
+            parameterizedAttributes: [
+                {
+                    attributeName: 'branch',
+                    jenkinsParameterName: 'BRANCH_TO_BUILD'
+                }
+            ]
         },
+        {
+            id: 'static_code_analysis',
+            displayName: 'Static Code Analysis',
+            eventName: 'static_code_analysis',
+            cronInterval: '*/1 * * * * *',
+            apiMethod: 'last_build_info',
 
-        // Multiple jenkins jobs
-        //{
-        //    id: 'static_code_analysis',
-        //    displayName: 'Static Code Analysis',
-        //    eventName: 'static_code_analysis',
-        //    cronInterval: '*/1 * * * * *',
-        //    apiMethod: 'last_build_info',
-        //
-        //    displayArguments: {
-        //        title_isEnabled: true,
-        //        buildNumber_isEnabled: false,
-        //        timeAgo_isEnabled: true,
-        //        branch_isEnabled: false,
-        //        displayDuration_isEnabled: true
-        //    }
-        //},
-        //{
-        //    id: 'install_latest',
-        //    displayName: 'Install Latest',
-        //    eventName: 'install_latest',
-        //    cronInterval: '*/1 * * * * *',
-        //    apiMethod: 'last_build_info',
-        //
-        //    externalBuildNumber: {
-        //       url: 'http://latest.your_application_url.host/build.txt'
-        //    },s
-        //
-        //    displayArguments: {
-        //        title_isEnabled: true,
-        //        buildNumber_isEnabled: true,
-        //        timeAgo_isEnabled: true,
-        //        branch_isEnabled: false,
-        //        displayDuration_isEnabled: true
-        //    }
-        //}
+            displayArguments: {
+                title_isEnabled: true,
+                buildNumber_isEnabled: false,
+                timeAgo_isEnabled: true,
+                branch_isEnabled: false,
+                displayDuration_isEnabled: true
+            }
+        },
+        {
+            id: 'install_latest',
+            displayName: 'Install Latest',
+            eventName: 'install_latest',
+            cronInterval: '*/1 * * * * *',
+            apiMethod: 'last_build_info',
+
+            externalBuildNumber: {
+               url: 'http://latest.your_application_url.host/build.txt'
+            },
+
+            displayArguments: {
+                title_isEnabled: true,
+                buildNumber_isEnabled: true,
+                timeAgo_isEnabled: true,
+                branch_isEnabled: false,
+                displayDuration_isEnabled: true
+            }
+        }
     ]
 }
